d759074e975bdc9df904bf3c92506c90a786c57b
xiaodong.li <2238033733@qq.com>
2025-11-11 10:18:52 +0800

    :recycle: refactor game scene and piece handling
    - Add `field` and `nextPiece` to `GameScene`
    - Update `Update` method to handle collisions and piece merging
    - Refactor `initCurrentPiece` to accept a piece and initialize `nextPiece`
    - Add `collides`, `mergePieceToField`, and `drawField` methods
    - Update `drawBlock` to use `colorm.DrawImageOptions` and adjust block drawing

--- game/block.go
+++ game/block.go
@@ -7,6 +7,7 @@ import (
 	"log"
 
 	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/colorm"
 	"github.com/hajimehoshi/ebiten/v2/examples/resources/images/blocks"
 )
 
@@ -22,14 +23,15 @@ var imageBlocks *ebiten.Image
 type BlockType int32
 
 const (
-	BlockTypeI BlockType = iota
+	BlockTypeNone BlockType = iota
+	BlockTypeI
 	BlockTypeJ
 	BlockTypeL
 	BlockTypeO
 	BlockTypeS
 	BlockTypeT
 	BlockTypeZ
-	BlockTypeMax
+	BlockTypeMax = BlockTypeZ
 )
 
 func init() {
@@ -43,12 +45,14 @@ func init() {
 // drawBlock draws a single block of the given type onto the target image.
 // x, y specify the drawing position in pixels (top-left corner).
 func drawBlock(r *ebiten.Image, block BlockType, x, y int) {
-	x0 := int(block) * blockWidth
+	x0 := (int(block) - 1) * blockWidth
 	x1 := x0 + blockWidth
 	img := imageBlocks.SubImage(image.Rect(x0, 0, x1, blockHeight)).(*ebiten.Image)
 
-	op := &ebiten.DrawImageOptions{}
+	op := &colorm.DrawImageOptions{}
 	op.GeoM.Translate(float64(x), float64(y))
 
-	r.DrawImage(img, op)
+	clr := colorm.ColorM{}
+
+	colorm.DrawImage(r, img, clr, op)
 }
--- game/gamescene.go
+++ game/gamescene.go
@@ -7,10 +7,11 @@ import (
 )
 
 type GameScene struct {
+	field         [fieldBlockCountY][fieldBlockCountX]BlockType
 	currentPiece  *Piece
 	currentPieceX int
 	currentPieceY int
-	frameCount    int64
+	nextPiece     *Piece
 }
 
 func NewGameScene() *GameScene {
@@ -19,47 +20,131 @@ func NewGameScene() *GameScene {
 
 func (g *GameScene) Update(i *Input) {
 	if g.currentPiece == nil {
-		g.initCurrentPiece()
+		g.initCurrentPiece(g.choosePiece())
+		return
 	}
+
+	oldBlocks := make([][]bool, len(g.currentPiece.blocks))
+	copy(oldBlocks, g.currentPiece.blocks)
 
 	if i.IsRotateRight() {
 		g.currentPiece.Rotate(t)
-	} else if i.IsRotateLeft() {
+		if g.collides(g.currentPieceX, g.currentPieceY) {
+			g.currentPiece.blocks = oldBlocks
+		}
+	}
+	if i.IsRotateLeft() {
 		g.currentPiece.Rotate(f)
-	} else if i.IsLeft() {
+		if g.collides(g.currentPieceX, g.currentPieceY) {
+			g.currentPiece.blocks = oldBlocks
+		}
+	}
+	if i.IsLeft() && !g.collides(g.currentPieceX-1, g.currentPieceY) {
 		g.currentPieceX--
-	} else if i.IsRight() {
+	}
+	if i.IsRight() && !g.collides(g.currentPieceX+1, g.currentPieceY) {
 		g.currentPieceX++
-	} else if i.IsDown() {
-		g.currentPieceY++
+	}
+	if i.IsDown() {
+		if !g.collides(g.currentPieceX, g.currentPieceY+1) {
+			g.currentPieceY++
+		} else {
+			g.mergePieceToField()
+			g.initCurrentPiece(g.nextPiece)
+		}
 	}
 }
 
 func (g *GameScene) Draw(r *ebiten.Image) {
+	// Draw static background and window frame
 	drawBackground(r)
 	r.DrawImage(imageWindows, nil)
 
+	// Draw field blocks
 	fieldX, fieldY := fieldWindowPosition()
+	g.drawField(r, fieldX, fieldY)
+
+	// Draw current falling piece
 	x := fieldX + g.currentPieceX*blockWidth
 	y := fieldY + g.currentPieceY*blockHeight
 	g.currentPiece.Draw(r, x, y)
+
+	// Draw next piece preview
+	nextX, nextY := nextWindowPosition()
+	x = nextX + blockWidth*(5-len(g.nextPiece.blocks))/2
+	y = nextY + blockHeight*(5-len(g.nextPiece.blocks))/2
+	g.nextPiece.Draw(r, x, y)
 }
 
-// initCurrentPiece initializes a new falling piece.
-// It selects a random piece, centers it horizontally,
-// and slightly shifts up if it's an I-type block.
-func (g *GameScene) initCurrentPiece() {
-	g.currentPiece = g.choosePiece()
+// initCurrentPiece sets the given piece as the current falling piece.
+func (g *GameScene) initCurrentPiece(piece *Piece) {
+	g.currentPiece = piece
 	g.currentPieceX = (fieldBlockCountX - len(g.currentPiece.blocks)) / 2
 	g.currentPieceY = 0
 
 	if g.currentPiece.blockType == BlockTypeI {
 		g.currentPieceY--
 	}
+
+	g.nextPiece = g.choosePiece()
 }
 
 func (g *GameScene) choosePiece() *Piece {
 	n := int(BlockTypeMax)
-	block := BlockType(rand.IntN(n))
+	block := BlockType(rand.IntN(n) + 1)
 	return Pieces[block]
+}
+
+// collides checks whether the piece collides with walls or placed blocks.
+func (g *GameScene) collides(px, py int) bool {
+	for i, row := range g.currentPiece.blocks {
+		for j, blocked := range row {
+			if !blocked {
+				continue
+			}
+
+			x := px + j
+			y := py + i
+
+			// Out of field bounds
+			if x < 0 || x >= fieldBlockCountX || y < 0 || y >= fieldBlockCountY {
+				return true
+			}
+
+			// Collides with placed block
+			if g.field[y][x] > BlockTypeNone {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+// mergePieceToField merges the current piece into the field grid.
+func (g *GameScene) mergePieceToField() {
+	for i, row := range g.currentPiece.blocks {
+		for j, blocked := range row {
+			if !blocked {
+				continue
+			}
+			x := g.currentPieceX + j
+			y := g.currentPieceY + i
+
+			if x >= 0 && x < fieldBlockCountX && y >= 0 && y < fieldBlockCountY {
+				g.field[y][x] = g.currentPiece.blockType
+			}
+		}
+	}
+}
+
+// drawField renders all placed blocks in the field grid.
+func (g *GameScene) drawField(r *ebiten.Image, x, y int) {
+	for i, row := range g.field {
+		for j, block := range row {
+			if block == BlockTypeNone {
+				continue
+			}
+			drawBlock(r, block, j*blockWidth+x, i*blockHeight+y)
+		}
+	}
 }
